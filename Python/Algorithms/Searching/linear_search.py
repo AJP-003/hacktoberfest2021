c07650f1b7c6cda482c6f13d27eaa5b6de72451a
Alex Joseph Pius <95560312+AJP-003@users.noreply.github.com>
2022-10-28 23:51:49 +0530

    Update linear_search.py

--- Python/Algorithms/Searching/linear_search.py
+++ Python/Algorithms/Searching/linear_search.py
@@ -3,14 +3,19 @@ def linear_search(lst,key):
     for i in lst:
         if i == key:
             print("Found at position {}".format(lst.index(i)+1))
-            flag = flag+1
+            flag =1
     if flag == 0:
         print("Not Found")
 
 
 # Driver Code
-my_list = [1,5,3,4]
-linear_search(my_list,5)
-linear_search(my_list,2)
+n=int(input("Enter the lenght of the list"))
+l=[]
+while True:
+    for i in range (0,n):
+        l.append(int(input))
+    n1=int(input("Enter the no. to be searched"))
+    linear_search(l,n1)
+
 
 
